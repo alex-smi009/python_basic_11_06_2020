96b4f4dde2e8b653f32071885db3b6683b0d9d10
alexey.smirnov <alex-smi009@yandex.ru>
2020-06-16 14:27:22 +0300

    HM lesson_1

--- homeworks/lesson1/task_4.py
+++ homeworks/lesson1/task_4.py
@@ -2,3 +2,18 @@
 Найдите самую большую цифру в числе.
 Для решения используйте цикл while и арифметические операции.
 '''
+print("Поиск самой большой цифры из числа")
+while True:
+    number = input("Введите целое положительное число\n")
+    if number.isdigit() and int(number) > 0:
+        number = int(number)
+        break
+    else:
+        print("Вы ввели некорректные данные")
+var_res = 0
+while number and var_res != 9:
+    temp = number % 10
+    if temp > var_res:
+        var_res = temp
+    number //= 10
+print("Наибольшая цифра в числе", var_res)
